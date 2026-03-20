918881aa46cf281d044845a9a980ab9e5657f0a0
Zyronon <zyronon@163.com>
2026-03-20 22:29:55 +0800

    fix

--- apps/nuxt/app/pages/(words)/practice-words/[id].vue
+++ apps/nuxt/app/pages/(words)/practice-words/[id].vue
@@ -804,6 +804,7 @@ useEvents([
   [ShortcutKey.ToggleDictation, () => (settingStore.dictation = !settingStore.dictation)],
   [ShortcutKey.ToggleTheme, toggleTheme],
   [ShortcutKey.ToggleConciseMode, toggleConciseMode],
+  [ShortcutKey.ToggleToolbar,  () => (settingStore.showToolbar = !settingStore.showToolbar)],
   [ShortcutKey.TogglePanel, () => (settingStore.showPanel = !settingStore.showPanel)],
   [ShortcutKey.RandomWrite, randomWrite],
 ])

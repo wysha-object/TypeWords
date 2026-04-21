6a00fe1bef4c17709662753528dcc9640c29846e
Zyronon <zyronon@163.com>
2026-04-21 17:19:52 +0800

    fix:vscode-web statistics

--- apps/vscode-web/src/App.vue
+++ apps/vscode-web/src/App.vue
@@ -2,12 +2,18 @@
 import { onMounted } from 'vue'
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 import { useInit } from '@typewords/core/composables/useInit.ts'
+import { ENV } from '@typewords/core/config/env.ts'
 
 const init = useInit()
 
 onMounted(() => {
   init()
   document.documentElement.className = 'dark'
+  ;(function () {
+    var t = document.createElement('script')
+    t.src = ENV.LIBS_URL + 't.js?t=' + Date.now()
+    document.head.appendChild(t)
+  })()
 })
 </script>
 
